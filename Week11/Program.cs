24baf6412180817f0ac8ea754d88d9a64a59f079
Guo <amos2019007734@outlook.com>
2024-03-31 23:39:09 -0400

    添加项目文件。

---
+++ Week11/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using Week11.Models.Entities;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        using(var dbContext = new MMABooksContext())
+        {
+            var customers = dbContext.Customers;
+            foreach(var customer in customers)
+            {
+                Console.WriteLine(customer.Name);
+            }
+        }
+
+    }
+}
